4a88ae0dcdb9aeb6a8d290922ab8e40444ec04ab
daimaruhoshizaki980-hub <daimaruhoshizaki980@gmail.com>
2026-06-09 15:46:31 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,1 +1,18 @@
-alert('bg.js loaded');
+window.addEventListener('load', function() {
+
+document.body.style.backgroundImage =
+"url('https://picsum.photos/1920/1080')";
+
+document.body.style.backgroundRepeat =
+"no-repeat";
+
+document.body.style.backgroundPosition =
+"center top";
+
+document.body.style.backgroundAttachment =
+"fixed";
+
+document.body.style.backgroundSize =
+"cover";
+
+});
