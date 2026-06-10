1a05327843544c15a55bead5fc57590c44d0d3f5
daimaruhoshizaki980-hub <daimaruhoshizaki980@gmail.com>
2026-06-10 13:00:20 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -4,20 +4,20 @@ window.addEventListener('load', function () {
     var left = document.createElement('img');
     left.src = 'https://daimaruhoshizaki980-hub.github.io/pworld-bg/左戦国.jpg';
     left.style.position = 'fixed';
-    left.style.left = '20px';
     left.style.top = '0';
+    left.style.left = 'calc(50% - 820px)';
     left.style.width = '420px';
-    left.style.zIndex = '999';
+    left.style.zIndex = '10';
     left.style.pointerEvents = 'none';
 
     // 右画像
     var right = document.createElement('img');
     right.src = 'https://daimaruhoshizaki980-hub.github.io/pworld-bg/右戦国.jpg';
     right.style.position = 'fixed';
-    right.style.right = '20px';
     right.style.top = '0';
+    right.style.left = 'calc(50% + 400px)';
     right.style.width = '420px';
-    right.style.zIndex = '999';
+    right.style.zIndex = '10';
     right.style.pointerEvents = 'none';
 
     document.body.appendChild(left);
