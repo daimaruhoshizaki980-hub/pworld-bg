65c9fc3e1e1d8815c6d94e453db7b4159bb696ed
daimaruhoshizaki980-hub <daimaruhoshizaki980@gmail.com>
2026-06-11 12:34:52 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,15 +1,52 @@
 window.addEventListener('load', function () {
 
-    var originalBg = getComputedStyle(document.body).backgroundImage;
+    // 左サイドメニュー
+    var leftMenu = document.getElementById('pwSideMenu-left');
 
-    document.body.style.backgroundImage =
-        'url("https://daimaruhoshizaki980-hub.github.io/pworld-bg/戦国背景.png"),' +
-        originalBg;
+    if (leftMenu) {
 
-    document.body.style.backgroundRepeat =
-        'no-repeat, repeat';
+        leftMenu.style.position = 'relative';
 
-    document.body.style.backgroundPosition =
-        'top center, center top';
+        var leftBg = document.createElement('img');
+
+        leftBg.src = 'https://daimaruhoshizaki980-hub.github.io/pworld-bg/左戦国.jpg';
+
+        leftBg.style.position = 'absolute';
+        leftBg.style.top = '0';
+        leftBg.style.left = '0';
+
+        leftBg.style.width = '500px';
+        leftBg.style.height = 'auto';
+
+        leftBg.style.zIndex = '-1';
+        leftBg.style.pointerEvents = 'none';
+
+        leftMenu.appendChild(leftBg);
+    }
+
+
+    // 右サイドメニュー
+    var rightMenu = document.getElementById('pwSideMenu-right');
+
+    if (rightMenu) {
+
+        rightMenu.style.position = 'relative';
+
+        var rightBg = document.createElement('img');
+
+        rightBg.src = 'https://daimaruhoshizaki980-hub.github.io/pworld-bg/右戦国.jpg';
+
+        rightBg.style.position = 'absolute';
+        rightBg.style.top = '0';
+        rightBg.style.right = '0';
+
+        rightBg.style.width = '500px';
+        rightBg.style.height = 'auto';
+
+        rightBg.style.zIndex = '-1';
+        rightBg.style.pointerEvents = 'none';
+
+        rightMenu.appendChild(rightBg);
+    }
 
 });
